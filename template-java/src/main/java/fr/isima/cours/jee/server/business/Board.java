3767feaa4ebb2df16e269952889d22561be6881b
Aurélien <untrucalacon@java.ce>
2019-04-07 15:54:34 +0200

    Ajout de la sauvegarde (savegarde a chaque tour mais pas plus utilisé)

--- template-java/src/main/java/fr/isima/cours/jee/server/business/Board.java
+++ template-java/src/main/java/fr/isima/cours/jee/server/business/Board.java
@@ -1,14 +1,7 @@
 package fr.isima.cours.jee.server.business;
 
 import java.io.*;
-import java.nio.charset.Charset;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.time.LocalDateTime;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Scanner;
 
 public class Board{
 	
